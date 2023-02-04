f98ca54b21308283d83c5c8e113a7af1f92db607
LunaticPython2003 <dasguptamadhurjya@gmail.com>
2023-02-04 22:08:48 +0530

    Added a pre defined Window size

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager
+from kivy.core.window import Window
 
+#Set pre defined window size
+Window.size = (600, 600)
 class Screen(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
