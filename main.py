462edb790f4fdf3300a8f5144d3cb1d1e5331582
LunaticPython2003 <dasguptamadhurjya@gmail.com>
2023-02-07 17:36:47 +0530

    Changed values to support new file structure

--- main.py
+++ main.py
@@ -7,28 +7,31 @@ from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
 
 #Set pre defined window size
 Window.size = (600, 600)
+
 class ClickableTextFieldRound(MDRelativeLayout):
     text = StringProperty()
     hint_text = StringProperty()
 
-class Screen(MDApp):
-    def __init__(self, **kwargs):
-        self.title = "Park-N-Go"
-        super().__init__(**kwargs)
+class WindowManager(ScreenManager):
+    pass
 
+class Park(MDApp):
     def build(self):
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(Builder.load_file("./Components/screen.kv"))
-        screen_manager.add_widget(Builder.load_file("./Components/login.kv"))
-        return screen_manager
+        self.screen = Builder.load_file("./Components/main.kv")
+        return self.screen
+
+    def hello(self):
+        # print(self.screen.get_screen('login').ids.text1.text)
+        print(self.screen.get_screen('login').ids.passw.text)
 
 if __name__=="__main__":
     LabelBase.register(name="MPoppins", fn_regular="assets/fonts/Poppins-Medium.ttf")
     LabelBase.register(name="BPoppins", fn_regular="assets/fonts/Poppins-SemiBold.ttf")
-Screen().run()
+Park().run()
 
 
                 
