4f6bcf2a981897bfdfb246b9f37fdbf4faf1f2a6
pooranjoyb <pooranjoyb2016@gmail.com>
2023-03-25 00:26:32 +0530

    Added saving functionality to save in database

--- main.py
+++ main.py
@@ -12,32 +12,38 @@ from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 
 load_dotenv()
-USER=os.getenv('USER')
+USER = os.getenv('USER')
 HOST = os.getenv('HOST')
 DATABASE = os.getenv('DATABASE')
 PASSWORD = os.getenv('PASSWORD')
 PORT = os.getenv('PORT')
 
 try:
-    mydb = ms.connect(host=HOST, user=USER, database=DATABASE, password=PASSWORD, port=PORT)
+    mydb = ms.connect(host=HOST, user=USER, database=DATABASE,
+                      password=PASSWORD, port=PORT)
     print("Connected to database")
+    mycursor = mydb.cursor()
 except:
     print("Cannot connect to database")
 
 
-#Set pre defined window size
+# Set pre defined window size
 Window.size = (600, 600)
+
 
 class WindowManager(ScreenManager):
     pass
 
+
 class Park_n_Go(MDApp):
     def build(self):
         self.screen = Builder.load_file("./Components/main.kv")
-        menu_items=[{"text": "Light Vehicle", "viewclass": "OneLineListItem", "on_release": lambda text="Light":self.display_text(text)}, 
-                    {"text": "Heavy Vehicle", "viewclass": "OneLineListItem", "on_release": lambda text="Heavy":self.display_text(text)},
-                    {"text":"Bicycle", "viewclass": "OneLineListItem", "on_release": lambda text="cycle":self.display_text(text)},
-                    {"text":"Three wheeler", "viewclass": "OneLineListItem", "on_release": lambda text="3-wheel":self.display_text(text)}]
+        menu_items = [{"text": "Light Vehicle", "viewclass": "OneLineListItem", "on_release": lambda text="Light": self.display_text(text)},
+                      {"text": "Heavy Vehicle", "viewclass": "OneLineListItem",
+                          "on_release": lambda text="Heavy": self.display_text(text)},
+                      {"text": "Bicycle", "viewclass": "OneLineListItem",
+                          "on_release": lambda text="cycle": self.display_text(text)},
+                      {"text": "Three wheeler", "viewclass": "OneLineListItem", "on_release": lambda text="3-wheel": self.display_text(text)}]
         self.menu = MDDropdownMenu(
             caller=self.screen.get_screen('mainscreen').ids.drop,
             items=menu_items,
@@ -45,12 +51,10 @@ class Park_n_Go(MDApp):
         )
 
         return self.screen
-    
-    def display_text(self,text):
+
+    def display_text(self, text):
         self.menu.dismiss()
         print(text)
-
-    
 
     def save(self):
 
@@ -59,12 +63,18 @@ class Park_n_Go(MDApp):
         name = self.screen.get_screen('mainscreen').ids.name
         phno = self.screen.get_screen('mainscreen').ids.phno
 
+        sql = "INSERT INTO Parking (Reg_no, Name, Phone_no) VALUES (%s, %s, %s)"
+        val = (regNo.text, name.text, phno.text)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+
         print(regNo.text, name.text, phno.text)
         print("Saved to Database")
 
     def DownloadReceipt(self):
         print("Downloadeded Receipt")
-    
+
     def showReceipt(self):
         print("Served receipt data to Screen")
 
@@ -100,7 +110,10 @@ class Park_n_Go(MDApp):
     def get_time(self, instance, time):
         print(time)
 
-if __name__=="__main__":
-    LabelBase.register(name="MPoppins", fn_regular="assets/fonts/Poppins-Medium.ttf")
-    LabelBase.register(name="BPoppins", fn_regular="assets/fonts/Poppins-SemiBold.ttf")
+
+if __name__ == "__main__":
+    LabelBase.register(
+        name="MPoppins", fn_regular="assets/fonts/Poppins-Medium.ttf")
+    LabelBase.register(
+        name="BPoppins", fn_regular="assets/fonts/Poppins-SemiBold.ttf")
 Park_n_Go().run()
