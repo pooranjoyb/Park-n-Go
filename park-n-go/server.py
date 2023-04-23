a74b7c80a21d9784115d7a2bee5766daae49b792
pooranjoyb <pooranjoyb2016@gmail.com>
2023-04-24 02:22:13 +0530

    Added multi-threading for generating pdf

---
+++ park-n-go/server.py
@@ -0,0 +1,25 @@
+import http.server
+import socketserver
+import webbrowser
+import time
+
+class start_serv:
+    def __init__(self, filename):
+        self.filename = filename
+    def start_server(self, stop_event):
+        PORT = 2611
+        Handler = http.server.SimpleHTTPRequestHandler
+
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("serving at port", PORT)
+            while not stop_event.is_set():
+                httpd.handle_request()
+                exit()
+                
+
+    def open_file(self):
+        print()
+        url = 'http://localhost:2611/' + self.filename
+        browser = webbrowser.get('windows-default')
+        browser.open(url)
+
